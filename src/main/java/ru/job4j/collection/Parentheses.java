5839f4df4534f42562855febabb5708b2f58366e
demidovv <demidov011@gmail.com>
2020-07-15 08:19:10 +0300

    Открытые и закрытые скобки 2

--- src/main/java/ru/job4j/collection/Parentheses.java
+++ src/main/java/ru/job4j/collection/Parentheses.java
@@ -2,21 +2,21 @@ package ru.job4j.collection;
 
 public class Parentheses {
     public static boolean valid(char[] data) {
-        if (data.length %2 == 0 && data.length != 0 && data[0] == '(') {
-            int open = 0;
-            int close =0;
-            for (char symbol: data) {
-                if (symbol == '(') {
-                    open++;
-                } else if (symbol == ')') {
-                    close++;
-                } else {
-                    return false;
-                }
+        int open = 0;
+        int close = 0;
+        for (char symbol : data) {
+            if (symbol == '(') {
+                open++;
+            } else if (symbol == ')') {
+                close++;
             }
-            if (open == close) {
-                return true;
+            if (open == 0 && close == 1) {
+                return false;
             }
+        }
+
+        if (open == close) {
+            return true;
         }
         return false;
     }
