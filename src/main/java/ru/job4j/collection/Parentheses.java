b1bde45d25c4e8040a37bbe2b6120e3eed0c1710
demidovv <demidov011@gmail.com>
2020-07-16 07:29:55 +0300

    Открытые и закрытые скобки 3

--- src/main/java/ru/job4j/collection/Parentheses.java
+++ src/main/java/ru/job4j/collection/Parentheses.java
@@ -2,22 +2,29 @@ package ru.job4j.collection;
 
 public class Parentheses {
     public static boolean valid(char[] data) {
-        int open = 0;
-        int close = 0;
-        for (char symbol : data) {
-            if (symbol == '(') {
-                open++;
-            } else if (symbol == ')') {
-                close++;
-            }
-            if (open == 0 && close == 1) {
-                return false;
-            }
-        }
+//        int open = 0;
+//        int close = 0;
+//        for (char symbol : data) {
+//            if (symbol == '(') {
+//                open++;
+//            } else if (symbol == ')') {
+//                close++;
+//            }
+//            if (open == 0 && close == 1) {
+//                return false;
+//            }
+//        }
+//
+//        if (open == close) {
+//            return true;
+//        }
+//        return false;
 
-        if (open == close) {
-            return true;
+        int count = 0;
+        for (int i = 0; i < data.length && count != -1; i++) {
+            count = data[i] == '(' ? count + 1: count;
+            count = data[i] == ')' ? count - 1: count;
         }
-        return false;
+        return count == 0;
     }
 }
