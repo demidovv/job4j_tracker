76c9c7cecbb2661b1c025003e7141eabab84c262
demidovv <demidov011@gmail.com>
2020-08-13 08:30:36 +0300

    3. Банковские переводы. [#279797]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -13,39 +13,80 @@ public class BankService {
      * @param user
      */
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<Account>());
-        }
+        users.putIfAbsent(user, new ArrayList<Account>());
+//
+//        if (!users.containsKey(user)) {
+//            users.put(user, new ArrayList<Account>());
+//        }
     }
 
     /**
      * Добавление нового счета пользователю
      * @param passport
      */
-    public void addAccount(String passport) {
+    public void addAccount(String passport, Account account) {
+//        users.putIfAbsent(findByPassport(passport), users.get(findByPassport(passport)).add(account));
 
-        findByPassport(passport);
+        if (!users.get(findByPassport(passport)).contains(account)) {
+            users.get(findByPassport(passport)).add(account);
+        }
     }
+
 
     /**
      * Поиск пользователя по паспорту
      * @param passport
-     * @return
+     * @return пользователь
      */
     public User findByPassport(String passport) {
-            for (User user: users.get()) {
-
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
             }
+        }
         return null;
     }
 
+    /**
+     * Поиск счета пользователя по реквизитам
+     * @param passport
+     * @param requisite
+     * @return аккаунт
+     */
     public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
         return null;
     }
 
+    /**
+     * Перевод денег
+     * @param srcPassport
+     * @param srcRequisite
+     * @param destPassport
+     * @param destRequisite
+     * @param amount
+     * @return
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
+        System.out.println(rsl);
         return rsl;
     }
 }
+
+
